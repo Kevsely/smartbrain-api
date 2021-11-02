05cc176917b7abe118b14b2e0c9d46b6962b93fb
Elegis Sossou <elegis.sossou@yahoo.com>
2021-11-02 12:59:23 +0100

    Adding a fake database for tests

--- server.js
+++ server.js
@@ -2,8 +2,36 @@ const express = require('express');
 
 const app = express();
 
+//Fake db waiting for the real one
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Jonh',
+            email: 'jonh@gmail.com',
+            password: 'cookies',
+            entries: 0, 
+            joined: new Date(), 
+        },
+        {
+            id: '123',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0, 
+            joined: new Date(), 
+        }
+    ]
+}
+
+//REQUEST
 app.get('/', (req, res) => {
     res.send('This is working');
+})
+
+//Signin - POST = Success/Fail
+app.post('/signin', (req, res) => {
+    res.json()
 })
 
 app.listen(3000, () => {
