0e4d53d822bd64acefc299764db34a8ac79aafbe
Elegis Sossou <elegis.sossou@yahoo.com>
2021-11-08 21:41:17 +0100

    Setting up signup with the db

--- server.js
+++ server.js
@@ -45,14 +45,20 @@ app.get('/', (req, res) => {
 
 //Signin - POST = Success/Fail
 app.post('/signin', (req, res) => {
-    //We're just checking for the first user. 
-    //We'll complete the functionnality when we'll create a database
-    //Probably a relationnal database
-    if(req.body.email === database.users[0].email 
-        && req.body.password === database.users[0].password)
-        res.json(database.users[0]);
-    else
-        res.status(400).json('rejected');
+    const {email, password} = req.body; 
+    db.select('email', 'hash').from('login').where('email', '=', email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        if(isValid) {
+            db.select('*').from('users').where('email', '=', email)
+            .then(user => res.json(user[0]))
+            .catch(err => res.status(400).json('Unable to get user'))
+        }
+        else {
+            res.status(400).json('Wrong credentials'); 
+        }
+    })
+    .catch(err => res.status(400).json('Error while logging in'))
 })
 
 //Register - POST = user
