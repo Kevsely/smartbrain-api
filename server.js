7aacad18614568f3810aee08efe33d7a47b23b73
Elegis Sossou <elegis.sossou@yahoo.com>
2021-11-10 09:24:14 +0100

    Moving profile handler to another branch

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const signin = require('./Controllers/SignIn');
-const register = require('./Controllers/Register')
+const register = require('./Controllers/Register'); 
+const profile = require('./Controllers/Profile');
+const image = require('./Controllers/Image');
 
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
@@ -29,31 +31,13 @@ app.get('/', (req, res) => {
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 
 //Register - POST = user
-app.post('/register', register.handleRegister(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt)); 
 
 //Profile/:userid - GET = user
-app.get('/profile/:id', (req, res) => {
-    const {id} = req.params;
-    db.select('*').from('users').where({id})
-    .then(user => {
-        if(user.length) res.json(user[0])
-        else res.status(400).json('No such user');
-    })
-    .catch(err => res.status(400).json('error getting user'));
-})
+app.get('/profile/:id', profile.handleProfile(db));
 
 //Image - PUT = user
-app.put('/image', (req, res) => {
-    const {id} = req.body;
-    db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        if(entries.length) res.json(entries[0])
-        // else res.status(400).json('No such user'); || Useless. A non-user can't upload image
-    }).catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', image.handleImage(db));
 
 app.listen(3001, () => {
     console.log('App is running correctly on port 3001');
